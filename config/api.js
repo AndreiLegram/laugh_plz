c11c74304b9960e4ce951e1ad79d6c328b222418
Andrei Legramante <57579770+AndreiLegram@users.noreply.github.com>
2020-05-09 22:52:30 -0300

    Delete api.js

--- config/api.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: '9TfRwNmkBUPsyc2adQkWv0I8e',
-    consumer_secret: '8ZWMnSzxu3y9QfneMrnwsC4Nme23lJazzgc5mwWSt8ZfPn7dKC',
-    access_token: '1257091494588153861-dyDW87DuO3r6rntOTFl6bCtzU6L1dg',
-    access_token_secret: 'x8k0TSBb4cXopxJlZnkVkqu1lQC0zhqI0odADvr5ngGuh'
-}
